80abaf1593b101bd2971fb5f7d93618b854bea9b
coldrain-f <intmain__@naver.com>
2026-07-20 20:59:38 +0900

    feat: add TreeView component with drag-and-drop functionality and customizable rendering

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
+import type { TreeDataItem } from "@/components/ui/tree-view";
+import { TreeView } from "@/components/ui/tree-view";
 
 export function App() {
 
@@ -20,6 +22,7 @@ export function App() {
 
   useEffect(() => {
     getDeckNames().then((names: string[]) => {
+      console.log(names);
       const formattedDeckNames = names.map(name => ({ label: name, value: name }));
       setDeckNames(formattedDeckNames);
     })
@@ -27,22 +30,59 @@ export function App() {
 
   }, []);
 
+  // TEST
+  const data: TreeDataItem[] = [
+    {
+      id: "1",
+      name: "Item 1",
+      children: [
+        {
+          id: "2",
+          name: "Item 1.1",
+          children: [
+            {
+              id: "3",
+              name: "Item 1.1.1",
+            },
+            {
+              id: "4",
+              name: "Item 1.1.2",
+            },
+          ],
+        },
+        {
+          id: "5",
+          name: "Item 1.2 (disabled)",
+          disabled: true,
+        },
+      ],
+    },
+    {
+      id: "6",
+      name: "Item 2 (draggable)",
+      draggable: true,
+    },
+  ];
+
   return (
-    <Select items={deckNames}>
-      <SelectTrigger className="w-full max-w-256">
-        <SelectValue />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Decks</SelectLabel>
-          {deckNames.map((deckName) => (
-            <SelectItem key={deckName.value} value={deckName.value}>
-              {deckName.value}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <>
+      <TreeView data={data} />
+      <Select items={deckNames}>
+        <SelectTrigger className="w-full max-w-256">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Decks</SelectLabel>
+            {deckNames.map((deckName) => (
+              <SelectItem key={deckName.value} value={deckName.value}>
+                {deckName.value}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </>
   )
 }
 
